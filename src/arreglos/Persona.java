e1af40f3ffa9e4e423d23174b8dd54603690fabb
Carlos Gurdian <111367949+CarlosG29@users.noreply.github.com>
2022-09-13 00:02:38 -0400

    Initial commit

---
+++ src/arreglos/Persona.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arreglos;
+
+/**
+ *
+ * @author Carlos Gurdian
+ */
+public class Persona {
+    private int ID;
+    private String Nombre;
+    private int precio;
+    private int existencia;
+
+    public Persona(int ID, String Nombre, int precio, int existencia) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+        this.precio = precio;
+        this.existencia = existencia;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+    
+    
+    
+}
